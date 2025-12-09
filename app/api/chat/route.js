750388277c8aed5beeae2b67d6a4e9a65b684445
Pierce Issah <PJIceskull2003@gmail.com>
2025-12-09 14:33:51 -0500

    add code for generating text from google ai API

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,9 +1,36 @@
-import { generateText } from "ai";
 import { google } from "@ai-sdk/google";
+import { generateText } from "ai";
 
 export async function POST(request) {
-  // AI Model
-  const model = google("gemini-2.5-flash", {
-    apiKey: process.env.GOOGLE_GENERATIVE_AI_API_KEY, // Import the API key from your env file
-  });
+  try {
+    const body = await request.json();
+    const language = body.language;
+    const code = body.code;
+
+    // AI Model
+    const model = google("gemini-2.5-flash", {
+      apiKey: process.env.GOOGLE_GENERATIVE_AI_API_KEY, // Import the API key from your env file
+    });
+    const prompt = `Generate documentation for the following ${language} code:\n\n${code}`;
+
+    // Generate Response
+    const generateResult = await generateText(model, prompt);
+
+    // Send result to frontend side
+    return new Response(
+      JSON.stringify({ documentation: generateResult.text }),
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.log("Error getting API:", error.message);
+
+    return new Response(
+      JSON.stringify({ error: error.message || JSON.stringify(err) }),
+      {
+        status: 500,
+      }
+    );
+  }
 }
